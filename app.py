7c92e4d6a108b522c4583ce9b2509621bd7558eb
SivaNisanthan <sivanisanthan2503@gmail.com>
2024-04-18 16:21:30 +0530

    Updated app.py and Home.html simple front end for deployment

--- app.py
+++ app.py
@@ -21,5 +21,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])  # To use the predict button in our web-app
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html', prediction_text='House Price should be $ {}'.format(output))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)  # Debug = True will reload the server each time we make changes in the code
